a139ff13fd870c58d9e4ab6bad9fae7f32fcba8b
Sikqo001 <125279894+Sikqo001@users.noreply.github.com>
2025-02-20 23:36:51 +0100

    Add files via upload

---
+++ snapchat-hacker 3/v0-user-next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
+
